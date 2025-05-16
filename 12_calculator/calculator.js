7c33d16d6aee0fdc07d6a1d93202d056f52a1dad
Scoppee <bhorluscope314@gmail.com>
2025-05-16 20:43:37 +0100

    Complete Calculator task

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,50 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if(array.length === 0){
+    return 0
+  }else{
+    return array.reduce((accumulator, currentValue) => 
+      accumulator + currentValue, 0
+    )
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  if(array.length === 0){
+    return 0
+  }else{
+    return array.reduce((accumulator, currentValue) => 
+      accumulator * currentValue, 1
+    )
+  }
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+  let result = 1
+	for (let i = 0; i < power; i++) {
+    result *= number
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if (number === 0 || number === 1){
+    return 1;
+  }else{
+    let result = 1
+    for (let i = 0; i < number; i++) {
+      result *= i+1
+    }
+    return result;
+  }
+
 };
 
 // Do not edit below this line
