3990c2f7c87cf7921bc7b7f4ad85bf917a4b42af
Scoppee <bhorluscope314@gmail.com>
2025-05-17 14:08:30 +0100

    Complete fibonacci task

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(member) {
+    if (member < 0){
+        return "OOPS";
+   }else{
+        let sequence = [0,1];
+        for (let i = sequence.length; i <= member; i++) {
+            sequence[i] = sequence[i-1] + sequence[i-2];
+        }
+        return sequence[member];
+   }
 };
 
 // Do not edit below this line
