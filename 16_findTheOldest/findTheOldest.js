5e819356a5e179719a1bab50999117a32ba71653
Scoppee <bhorluscope314@gmail.com>
2025-05-17 16:17:34 +0100

    Complete findTheOldest task

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    let eldest = array.reduce((accumulator, currentvalue) => {
+        if(accumulator.yearOfDeath === undefined){
+            accumulator["yearOfDeath"] = new Date().getFullYear()     
+        }
+        if(currentvalue.yearOfDeath === undefined){
+            currentvalue["yearOfDeath"] = new Date().getFullYear()        
+        }
+        return (accumulator.yearOfDeath - accumulator.yearOfBirth) > (currentvalue.yearOfDeath - currentvalue.yearOfBirth) ? accumulator : currentvalue;
+    })
+    return eldest
 };
 
 // Do not edit below this line
