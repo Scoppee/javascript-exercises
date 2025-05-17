3098c6dc5067b88d15cc2a58b46ef8d72d62f9b0
Scoppee <bhorluscope314@gmail.com>
2025-05-17 13:36:02 +0100

    Complete Palindrome test

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let wordWithoutPunct = word.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+    let newString = "";
+    for (let i = wordWithoutPunct.length - 1; i >= 0; i--) {
+        newString += wordWithoutPunct[i]; 
+    }
+   
+    return newString === wordWithoutPunct;
 };
 
 // Do not edit below this line
